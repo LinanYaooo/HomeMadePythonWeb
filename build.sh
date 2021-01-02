f03e7e9a37aaa11c515e7e8a823a464f026668e2
linany <ieyaolinan@gmail.com>
2021-01-03 01:32:30 +0800

    asdasd

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 pip3 install redis==3.5.3
 pip3 install PyYAML==5.3.1
 pip3 install flask==1.1.2
-pip3 install psycopg2
+pip3 install psycopg2==2.8.6 -i https://mirrors.aliyun.com/pypi/simple/
 pip3 install DButils==2.0
 
 # 启动任务
