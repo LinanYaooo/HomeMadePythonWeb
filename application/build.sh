1a45d680798592e04a70238724d5ea5e42e48bee
ieyaolinan <ieyaolinan@163.com>
2021-01-08 11:50:29 +0800

    我change buildâ

--- application/build.sh
+++ application/build.sh
@@ -3,4 +3,4 @@ echo "构建启动"
 echo $(pwd)
 
 # 启动任务 - 通过 root 权限启动
-nohup gunicorn3 -b 0.0.0.0:81 --workers=4 manage:app > /home/ubuntu/guncorn.log 2>&1 &
+nohup gunicorn3 -b 0.0.0.0:81 --workers=4 application.manage:app > /home/ubuntu/guncorn.log 2>&1 &
