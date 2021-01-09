7af16ec7283aececdf1b3a003600bdc2fae95137
linany <ieyaolinan@gmail.com>
2021-01-09 16:39:14 +0800

    try

--- test/buildDependency.sh
+++ test/buildDependency.sh
@@ -2,12 +2,12 @@
 echo "构建"
 
 # 依赖安装
-pip3 install redis==3.5.3
-pip3 install PyYAML==5.3.1
-pip3 install flask==1.1.2
-pip3 install psycopg2==2.8.6
-pip3 install DBUtils==2.0
-pip3 install pandas==1.2.0
+sudo pip3 install redis==3.5.3
+sudo pip3 install PyYAML==5.3.1
+sudo pip3 install flask==1.1.2
+sudo pip3 install psycopg2==2.8.6
+sudo pip3 install DBUtils==2.0
+sudo pip3 install pandas==1.2.0
 
 # 打印必要环境变量
 echo $PG_PWD
